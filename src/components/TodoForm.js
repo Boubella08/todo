e9050b45f7eab68816e85f5de9ee3711735028a2
Boubella08 <mahdiboubella1@gmail.com>
2023-12-25 03:32:06 +0100

    update

---
+++ src/components/TodoForm.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react'
+
+export const TodoForm = ({addTodo}) => {
+    const [value, setValue] = useState('');
+
+    const handleSubmit = (e) => {
+      // prevent default action
+        e.preventDefault();
+        if (value) {
+          // add todo
+          addTodo(value);
+          // clear form after submission
+          setValue('');
+        }
+      };
+  return (
+    <form onSubmit={handleSubmit} className="TodoForm">
+    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='What is the task today?' />
+    {/* <button type="submit" className='todo-btn'>Add Task</button> */}
+
+
+    <div className='dd'>
+    <button type="submit">
+  <span>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z"></path></svg> Add
+  </span>
+</button>
+</div>
+  </form>
+  )
+}
